362054d11f6f552f00cb205adbde1562e6f30293
Niam Moltta <lastralab@gmail.com>
2017-09-29 11:52:25 -0300

    Add files via upload

---
+++ LCD_shield.ino
@@ -0,0 +1,37 @@
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.setCursor(0,0);
+  lcd.print("LCD Key Shield");
+  lcd.setCursor(0,1);
+  lcd.print("Press Key:");
+}
+
+void loop() {
+  int x;
+  x = analogRead (0);
+  lcd.setCursor(10,1);
+  if (x < 60) {
+    lcd.print ("Right ");
+  }
+  else if (x < 200) {
+    lcd.print ("Up    ");
+  }
+  else if (x < 400){
+    lcd.print ("Down  ");
+  }
+  else if (x < 600){
+    lcd.print ("Left  ");
+  }
+  else if (x < 800){
+    lcd.print ("Select");
+  }
+}
+
