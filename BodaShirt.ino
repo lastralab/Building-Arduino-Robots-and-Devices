45d0ec07e22aa2bdc9d2bd206189ca636328f404
Niam Moltta <lastralab@gmail.com>
2017-09-29 08:20:00 -0300

    Update BodaShirt.ino

--- BodaShirt.ino
+++ BodaShirt.ino
@@ -1,3 +1,7 @@
+// Design to print on a t-shirt for our wedding
+// Feb - 2017
+// Las Cruces, NM.
+
 #define Love 13
 #define Patient 1
 #define Adventurous 1
@@ -6,7 +10,7 @@
 #define AthleticBodyType 1 //LOL
 #define TeamWork 10
 
-int Tania = (Patient + Adventurous + Trustworthy + Stubborn + AthleticBodyType);
+int Niam = (Patient + Adventurous + Trustworthy + Stubborn + AthleticBodyType);
 int Grace = (Patient + Adventurous + Trustworthy + Stubborn + AthleticBodyType);
 
 void setup() {
