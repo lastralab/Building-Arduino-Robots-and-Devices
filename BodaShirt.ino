eadfff8cd10431d5daef9d114bc20f9221ae0dcf
Niam Moltta <lastralab@gmail.com>
2018-06-15 10:08:12 -0600

    Update BodaShirt.ino

--- BodaShirt.ino
+++ BodaShirt.ino
@@ -1,5 +1,5 @@
-// Design to print on a t-shirt for our wedding
-// Feb - 2017
+// Dripping Springs Natural Area
+// 04 Feb - 2017
 // Las Cruces, NM.
 
 #define Love 13
@@ -7,7 +7,7 @@
 #define Adventurous 1
 #define Trustworthy 1
 #define Stubborn 1
-#define AthleticBodyType 1 //LOL
+#define AthleticBodyType 1 // LOL
 #define TeamWork 10
 
 int Niam = (Patient + Adventurous + Trustworthy + Stubborn + AthleticBodyType);
@@ -24,7 +24,7 @@ void loop() {
   
   while(Tania + Grace == TeamWork) {
     
-    digitalWrite(Love, HIGH);   //LIGHT ON 
+    digitalWrite(Love, HIGH);   // LIGHT ON 
       
   }
 }
